a5094948fb66f7f1aabed0e3e92e054ad3cc857e
rona <pub.ronalyn.taguiam@gmail.com>
2023-09-26 14:34:54 +0800

    add project setup

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# tldraw-challenge
+# tldraw Extension
+tldraw is a collaborative digital whiteboard available at tldraw.com. 
+
+## Installation
+Run to install all packages needed in this project.
+```
+npm install
+```
+
+## Development
+Start the app in local development environment. Run the project using this link: `http://localhost:8080/`
+```
+npm start
+```
+
+## Build
+Create a build
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.tsx"),
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
+          }
+        }
+      },
+      {
+        test:/\.css$/,
+        use:["style-loader","css-loader"]
+      },
+      {
+        test:/\.(ico|png|svg|jpg|gif)$/,
+        use:["file-loader"]
+      }
+    ]
+  },
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+}
